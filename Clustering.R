6c5037832e1dfb54fc11d0d563d6967fb050ee2f
Frithjof Sanger <fsanger@andrew.cmu.edu>
2024-06-11 15:08:41 -0400

    Pittsburgh Penguins Clustering by (x, y) coordinate

--- Clustering.R
+++ Clustering.R
@@ -2,12 +2,15 @@ library(tidyverse)
 nhl_shots <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/nhl_shots.csv")
 library(ggplot2)
 
-init_kmeans <- nhl_shots |>
+nhl_shots_penguins = subset(nhl_shots, teamCode == "PIT")
+
+
+init_kmeans <- nhl_shots_penguins |>
   select(xCordAdjusted, yCordAdjusted) |>
   kmeans(algorithm = "Lloyd", centers = 5, nstart = 1)
 
-nhl_shots |>
+nhl_shots_penguins |>
   mutate(shot_clusters = as.factor(init_kmeans$cluster)) |>
   ggplot(aes(x = xCordAdjusted, y = yCordAdjusted, 
              color = shot_clusters)) + 
-  geom_point()
+  geom_point(alpha = 0.5)
