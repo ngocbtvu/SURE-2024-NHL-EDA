66e8c3662ee67c9d4e6b5ce6451106b02bb6f8dc
Frithjof Sanger <fsanger@andrew.cmu.edu>
2024-06-12 14:35:16 -0400

    Added code at the bottom for the 32 team clustering chart, and made 3 potential data frames to run it with.

--- Clustering.R
+++ Clustering.R
@@ -4,13 +4,46 @@ library(ggplot2)
 
 nhl_shots_penguins = subset(nhl_shots, teamCode == "PIT")
 
-
-init_kmeans <- nhl_shots_penguins |>
+##Shots cluster chart
+init_kmeans <- nhl_shots |>
   select(xCordAdjusted, yCordAdjusted) |>
   kmeans(algorithm = "Lloyd", centers = 5, nstart = 1)
 
-nhl_shots_penguins |>
+nhl_shots |>
   mutate(shot_clusters = as.factor(init_kmeans$cluster)) |>
   ggplot(aes(x = xCordAdjusted, y = yCordAdjusted, 
-             color = shot_clusters)) + 
+             color = shot_clusters, shape = teamCode)) + 
   geom_point(alpha = 0.5)
+
+##Team names cluster plot code
+#shooting average time on ice
+team_ave_shots = nhl_shots_5v5 |>
+  group_by(teamCode) |>
+  summarize(aveTOI = mean(shootingTeamAverageTimeOnIce), 
+            ave_xGoal = mean(xGoal))
+
+#average rest difference
+team_ave_shots.2 = nhl_shots_5v5 |>
+  group_by(teamCode) |>
+  summarize(ARD = mean(averageRestDifference), 
+            ave_xGoal = mean(xGoal))
+
+#expected rebounds
+team_ave_shots.3 = nhl_shots_5v5 |>
+  group_by(teamCode) |>
+  summarize(ave_xRebounds = mean(xRebound), 
+            ave_xGoal = mean(xGoal))
+
+init_ave_kmeans <- team_ave_shots.3 |>
+  select(ave_xGoal, ave_xRebounds) |>
+  kmeans(algorithm = "Lloyd", centers = 5, nstart = 1)
+
+team_ave_shots.3 |>
+  mutate(shot_clusters = as.factor(init_ave_kmeans$cluster)) |>
+  ggplot(aes(x = ave_xGoal, y = ave_xRebounds, 
+             color = shot_clusters)) + 
+  geom_text(label = team_ave_shots.3$teamCode)
+  geom_point()
+
+
+
